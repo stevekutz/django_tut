49c617a50a11cc7ee04e7879ca7abac24652f59a
stevekutz <stkutz@gmail.com>
2020-05-03 19:58:30 -0500

    updated index view, added detail,results, & vote view

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = "index"),
+    path('', views.index, name = "index"),  #/polls/
+    path('<int:question_id>/', views.detail, name = 'detail'), #/polls/4/
+    path('<int:question_id>/results/', views.results, name = 'results'), #/polls/2/results/
+    path('<int:question_id>/vite/', views.vote, name = 'vote'), #/polls/3/vote/
 
 ]
