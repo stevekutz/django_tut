9bb4870507a8a29d72f0619339d04a5aa0c5ada2
stevekutz <stkutz@gmail.com>
2020-05-06 17:59:56 -0500

    updated results view and created results template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -16,8 +16,9 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You are looking at the results of question %s. "
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/results.html', context)
 
 def vote(request, question_id):
     question = get_object_or_404 (Question, pk=question_id)
