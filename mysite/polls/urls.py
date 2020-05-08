e54a0a9102c2ae549d89c08f3741793bfd53fd4e
stevekutz <stkutz@gmail.com>
2020-05-07 21:22:59 -0500

    modified urls.py to implement generic views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,9 +3,8 @@ from django.urls import path
 from . import views
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name = "index"),  #/polls/
-    path('<int:question_id>/', views.detail, name = 'detail'), #/polls/4/
-    path('<int:question_id>/results/', views.results, name = 'results'), #/polls/2/results/
+    path('', views.IndexView.as_view, name = "index"),  #/polls/
+    path('<int:pk>/', views.DetailView.as_view, name = 'detail'), #/polls/4/
+    path('<int:pk>/results/', views.ResultsView.as_view, name = 'results'), #/polls/2/results/
     path('<int:question_id>/vite/', views.vote, name = 'vote'), #/polls/3/vote/
-
 ]
