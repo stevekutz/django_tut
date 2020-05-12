0803a0e62bb8835729ea03f6d4bf3f048b9b71d1
stevekutz <stkutz@gmail.com>
2020-05-12 18:58:50 -0500

    experimented with backround image, resolved static file directory structure

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -116,6 +116,6 @@ class QuestionDetailViewTests(TestCase):
         The detail view of a question with a pub_date in the past displays the question's text
         """
         past_question = create_question(question_text = 'Past Question.', days =- 5)
-        url = reverse('polls:detail', args = (past_question.id))
+        url = reverse('polls:detail', args = (past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
