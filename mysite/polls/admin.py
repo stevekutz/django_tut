c21229e81b330e77c43aa9c3cadff0a90b32bce7
stevekutz <stkutz@gmail.com>
2020-05-14 18:32:06 -0500

    refactored admin.py

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Choice, Question
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        ('Questin Text Field',  {'fields': ['question_text']}),
-        ('Date information', {'fields':['pub_date']}),
+        ('Question Text Field',  {'fields': ['question_text']}),
+        ('Date information', {'fields':['pub_date'], 'classes': ['collapse']}),
     ]
+
+    inlines = [ChoiceInline]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
 
 admin.site.register(Question, QuestionAdmin)    
 
